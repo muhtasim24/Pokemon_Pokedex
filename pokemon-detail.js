7d135ff81ad194708a7d341ef5a282980a997588
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-12-02 09:52:04 -0500

    array of colors for pokemon's types, will use this to change the background according to type

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -62,3 +62,31 @@ async function loadPokemon(id) {
         return false;
     }
 }
+
+async function navigatePokemon(id) {
+    currentPokemonId = id;
+    await loadPokemon(id);
+}
+
+// change the background of text, based on the type of the pokemon
+const typeColors = {
+    normal: "#A8A878",
+    fire: "#F08030",
+    water: "#6890F0",
+    electric: "#F8D030",
+    grass: "#78C850",
+    ice: "#98D8D8",
+    fighting: "#C03028",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    flying: "#A890F0",
+    psychic: "F85888",
+    bug: "#A8B820",
+    rock: "#B8A038",
+    ghost: "#705898",
+    dragon: "#7038F8",
+    dark: "#705848",
+    steel: "#B8B8D0",
+    dark: "#EE99AC",
+};
+
