dff9e0d26a66f78b669505755d722dba41a59578
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-12-02 10:04:34 -0500

    Implemented function to retrieve pokemons type and set elements based on the color of that type

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -90,3 +90,46 @@ const typeColors = {
     dark: "#EE99AC",
 };
 
+// attaching the element styles to all the elements 
+function setElementStyles(elements, cssProperty, value) {
+    elements.forEach((element) => {
+        element.style[cssProperty] = value;
+    });
+}
+
+function rgbaFromHex(hexColor) {
+    return [
+        parseInt(hexColor.slice(1,3), 16),
+        parseInt(hexColor.slice(3,5), 16),
+        parseInt(hexColor.slice(5,7), 16),  
+    ].join(", "); // give our color a transluent effect towards the end of each stats bar
+}
+
+function setTypeBackgroundColor(pokemon) {
+    const mainType = pokemon.types[0].type.name; // get the type of the pokemon
+    const color = typeColors[mainType]; // use that type to access the color from our typeColors array
+
+    if (!color) {
+        console.warn(`Color not defined for type: ${mainType}`);
+    }
+
+    const detailMainElement = document.querySelector(".detail-main");
+    setElementStyles([detailMainElement], "backgroundColor", color);
+    setElementStyles([detailMainElement], "borderColor", color);
+    setElementStyles(document.querySelectorAll(".power-wrapper > p"), "backgroundColor", color);
+    setElementStyles(document.querySelectorAll(".stats-wrap p.stats"), "color", color);
+    setElementStyles(document.querySelectorAll(".stats-wrap .progress-bar"), "color", color);
+
+    const rgbaColor = rgbaFromHex(color);
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = `
+        .stats-wrap .progress-bar::-webkit-progress-bar {
+            background-color: rgba(${rgbaColor}, 0.5);
+        }
+        .stats-wrap .progress-bar::-webkit-progress-value {
+            background-color: ${color};
+        }
+    `;
+
+    document.head.appendChild(styleTag);
+}
