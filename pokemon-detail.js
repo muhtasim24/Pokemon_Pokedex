21312898c405975b0e742aecc1ce787c24b188fb
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-12-04 22:38:32 -0500

    Search bar fits full width

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -175,8 +175,8 @@ function displayPokemonDetails(pokemon) {
         });
     });
 
-    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight").textContent = `${weight / 10} kg`;
-    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height").textContent = `${height / 10} m`;
+    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight").textContent = `${weight / 10}kg`;
+    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height").textContent = `${height / 10}m`;
 
     const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail.move");
     abilities.forEach(({ ability }) => {
