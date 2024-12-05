800e6145063da957f3de7b030054d5230b603acd
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-12-04 22:34:45 -0500

    Details page done, finished most of CSS, got pokedex to look appealing and work, also fixed some bugs in details.html file

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -30,7 +30,7 @@ async function loadPokemon(id) {
     if (currentPokemonId === id){
         displayPokemonDetails(pokemon);
         const flavorText = getEnglishFlavorText(pokemonSpecies);
-        document.querySelector("body3-fonts.pokemon-description").textContent = flavorText;
+        document.querySelector(".body3-fonts.pokemon-description").textContent = flavorText;
     
     const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) => 
         document.querySelector(sel)
@@ -45,7 +45,7 @@ async function loadPokemon(id) {
                 navigatePokemon(id - 1);
             });
         }
-        if (id !== MAX_POKEMON) {
+        if (id !== 151) {
             rightArrow.addEventListener("click", () => {
                 navigatePokemon(id + 1);
             })
@@ -154,15 +154,16 @@ function displayPokemonDetails(pokemon) {
 
     document.querySelector("title").textContent = capitalizePokemonName;
 
-    const detailMainElement = document.querySelector("detail-main");
+    const detailMainElement = document.querySelector(".detail-main");
     detailMainElement.classList.add(name.toLowerCase());
 
-    document.querySelector("name-wrap .name").textContent = capitalizePokemonName;
+    document.querySelector(".name-wrap .name").textContent = capitalizePokemonName;
 
     document.querySelector(".pokemon-id-wrap .body2-fonts").textContent =  `#${String(id).padStart(3, "0")}`;
 
     const imageElement = document.querySelector(".detail-img-wrapper img");
-    imageElement.src = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/other/dream-world/${id}.svg`;
+    imageElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    imageElement.alt = name;
 
     const typeWrapper = document.querySelector(".power-wrapper");
     typeWrapper.innerHTML = "";
@@ -171,6 +172,63 @@ function displayPokemonDetails(pokemon) {
             createAndAppendElement(typeWrapper, "p", {
             className:`body3-fonts type ${type.name}`,
             textContent: type.name, 
-        })
+        });
     });
+
+    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight").textContent = `${weight / 10} kg`;
+    document.querySelector(".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height").textContent = `${height / 10} m`;
+
+    const abilitiesWrapper = document.querySelector(".pokemon-detail-wrap .pokemon-detail.move");
+    abilities.forEach(({ ability }) => {
+        createAndAppendElement(abilitiesWrapper, "p", {
+            className: "body3-fonts",
+            textContent: ability.name,
+        });
+    });
+
+    const statsWrapper = document.querySelector(".stats-wrapper");
+    statsWrapper.innerHTML = "";
+
+    const statNameMapping = {
+        hp: "HP",
+        attack: "ATK",
+        defense: "DEF",
+        "special-attack": "SATK",
+        "special-defense": "SDEF",
+        speed: "SPD",
+    };
+
+    stats.forEach(({stat, base_stat}) => {
+        const statDiv = document.createElement("div");
+        statDiv.className = "stats-wrap";
+        statsWrapper.appendChild(statDiv);
+
+        createAndAppendElement(statDiv, "p", {
+            className: "body3-fonts stats",
+            textContent: statNameMapping[stat.name],
+        });
+
+        createAndAppendElement(statDiv, "p", {
+            className: "body3-fonts",
+            textContent: String(base_stat).padStart(3, "0"),
+        });
+
+        createAndAppendElement(statDiv, "progress", {
+            className: "progress-bar",
+            value: base_stat,
+            max: 100,
+        });
+    });
+
+    setTypeBackgroundColor(pokemon);
+}
+
+function getEnglishFlavorText(pokemonSpecies) {
+    for (let entry of pokemonSpecies.flavor_text_entries){
+        if (entry.language.name === "en") {
+            let flavor = entry.flavor_text.replace(/\f/g, " ");
+            return flavor;
+        }
+    }
+    return "";
 }
