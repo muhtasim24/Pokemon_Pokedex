4202e28f1d5fa34807a7c1a35970ea0d09c624db
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-12-16 23:55:40 -0500

    retrieve DOM elements

--- quiz.js
+++ quiz.js
@@ -1,1 +1,10 @@
-console.log('test');
+// Retrieve DOM elements
+// to manipulate html elements, need to access them in JS
+
+const resultElement = document.getElementById('result');
+const pokemonImage = document.getElementById('pokemonImage');
+const optionsContainer = document.getElementById('options');
+const pointsElement = document.getElementById('pointsValue');
+const totalCount = document.getElementById('totalCount');
+const mainContainer = document.getElementById('container');
+const loadingContainer=document.getElementById('loadingContainer');
