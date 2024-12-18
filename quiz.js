1bdb8f2902aa6b5af53952716f68a6794f9fb580
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-12-17 20:21:31 -0500

    start on js and added a little more styling

--- quiz.js
+++ quiz.js
@@ -1,10 +1,9 @@
 // Retrieve DOM elements
 // to manipulate html elements, need to access them in JS
 
-const resultElement = document.getElementById('result');
-const pokemonImage = document.getElementById('pokemonImage');
-const optionsContainer = document.getElementById('options');
-const pointsElement = document.getElementById('pointsValue');
-const totalCount = document.getElementById('totalCount');
-const mainContainer = document.getElementById('container');
-const loadingContainer=document.getElementById('loadingContainer');
+let gameData;
+const main = document.querySelector('main');
+const playBtn = document.querySelector('play');
+
+playBtn.addEventListener('click', fetchData);
+
